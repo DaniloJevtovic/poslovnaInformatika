efc5616591794f7552630152c759f38b90459c18
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2017-07-07 22:16:33 +0200

    prepravljen prikaz, ubacena validacija naseljenog mjesta

---
+++ project/app/models/constants/KonstanteNaseljenogMjesta.java
@@ -0,0 +1,8 @@
+package models.constants;
+
+public class KonstanteNaseljenogMjesta {
+	public static final int MAKSIMALNA_DUZINA_OZNAKA = 3;
+	public static final int MAKSIMALNA_DUZINA_NAZIV = 120;
+	public static final int MINIMALNA_DUZINA_PTT = 5;
+	public static final int MAKSIMALNA_DUZINA_PTT = 12;
+}
